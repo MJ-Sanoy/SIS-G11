a7b4ba2f64c615b8944aa4a6ad63a056ecf28317
MJ-Sanoy <sanoymj4820@gmail.com>
2025-03-28 01:04:24 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,7 @@
 <?php
     $username="root";
     $password=""; // CHANGE PASSWORD HERE
-    $server="localhost:8080"; // CHANGE THESE PORTS SA SCHOOL
+    $server=""; // CHANGE THESE PORTS SA SCHOOL
     $db_name="sis";
     $conn = new mysqli ($server, $username, $password, $db_name);
     if ($conn->connect_error) {
